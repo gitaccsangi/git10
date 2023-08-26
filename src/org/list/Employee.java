3d052154cccb7737a7110f8be92c75240e1f859f
gitaccsangi <sangeethakaviya1994@gmail.com>
2023-08-26 11:43:13 +0530

    firstlist

---
+++ src/org/list/Employee.java
@@ -0,0 +1,13 @@
+package org.list;
+
+public class Employee{ 
+	
+	private void Employee() {
+		System.out.println("run");
+	}
+public static void main(String[] args) {
+	new Employee();
+	
+}
+}
+
---
+++ src/org/list/evenNumber.java
@@ -0,0 +1,15 @@
+package org.list;
+
+public class evenNumber{
+	
+	public void m1(double x) {
+		System.out.println("double:" +x);
+	}
+	public void m1(int a) {
+		System.out.println("int :" +a);
+	}
+	
+public static void main(String[] args) {
+	
+}
+}
